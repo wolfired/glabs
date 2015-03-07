9cbe3a1825f3668ce4cb014bcc5a0bcf4de00370
LinkWu <wjianl@qq.com>
2015-03-07 18:01:42 +0800

    test code

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+#include "math.h"
+
+
+
+int main(int argc, char **argv){
+	std::cout << add(1, 2);
+	return 0;
+}
---
+++ src/math.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H
+
+#define MATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int x, int y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
